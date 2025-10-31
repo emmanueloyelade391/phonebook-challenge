46e8af44ec3dcdef3362b2734e37543c7ec1b817
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-10-30 21:18:21 -0400

    Add links to every single page that links to every other page in the website

--- src/pages/Page10.jsx
+++ src/pages/Page10.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
 import "../App.css";
 import profile from "../images/profile.png";
 import apple from "../images/apple.png";
@@ -103,6 +104,19 @@ export default function Page10() {
                         )
                     })}
                 </div>
+                <nav>
+                    <Link to="/Allpages">All Pages</Link> | 
+                    <Link to="/page1">Page 1</Link> |
+                    <Link to="/page2">Page 2</Link> |
+                    <Link to="/page3">Page 3</Link> |
+                    <Link to="/page4">Page 4</Link> |
+                    <Link to="/page5">Page 5</Link> |
+                    <Link to="/page6">Page 6</Link> |
+                    <Link to="/page7">Page 7</Link> |
+                    <Link to="/page8">Page 8</Link> |
+                    <Link to="/page9">Page 9</Link> |
+                    <Link to="/page10">Page 10</Link> |
+                </nav>
             </section>
 
             {/*This section allows the user to add new contacts by adding a new name, phone number, and email address. 
